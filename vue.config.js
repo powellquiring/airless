777a9952fbe0a30123835af4adb2ead210a5c3e7
Powell Quiring <powellquiring@gmail.com>
2025-08-06 08:10:58 -0700

    DNS config 2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/airless/' : '/'
+  // without DNS publicPath: process.env.NODE_ENV === 'production' ? '/airless/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
 })
